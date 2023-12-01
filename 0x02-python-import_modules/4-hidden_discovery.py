7041df3f1a1d6bc6debdcb357069485a5e0c67f1
imanimk <imanimkingsley@gmail.com>
2023-12-01 18:39:19 +0300

    import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,17 @@
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import hidden_4
+    import code
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    # Create an interactive namespace using code.InteractiveConsole
+    console = code.InteractiveConsole(locals=vars(hidden_4))
+
+    # Get all names in the namespace
+    all_names = console.locals.keys()
+
+    # Filter and print names that don't start with '__'
+    filtered_names = [name for name in sorted(all_names) if not name.startswith('__')]
+
+    for name in filtered_names:
+        print(name)
+
